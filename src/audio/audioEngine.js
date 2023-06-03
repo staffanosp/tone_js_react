f418d440117498a4dcd4db09af522aa0713ecd6f
Staffan Ösp <staffan.osp@gmail.com>
2023-06-03 10:42:41 +0200

    support for different sized chords

--- src/audio/audioEngine.js
+++ src/audio/audioEngine.js
@@ -4,147 +4,166 @@ import { clamp } from "../utils/utils";
 import kickSample from "../../public/sounds/kick.wav";
 
 function createAudioEngine(numOscillators = 5) {
-	console.log("—— AUDIO ENGINE: CREATE ——");
+  console.log("—— AUDIO ENGINE: CREATE ——");
 
-	// Create the kick player
-	const kickPlayer = new Tone.Player(kickSample).toDestination();
+  // Create the kick player
+  const kickPlayer = new Tone.Player(kickSample).toDestination();
 
-	// Set the BPM to 120
-	Tone.Transport.bpm.value = 120;
+  // Set the BPM to 120
+  Tone.Transport.bpm.value = 120;
 
-	//create oscillator + gain nodes
-	const oscillatorNodes = [];
-	const oscillatorGainNodes = [];
-	const panners = [];
+  //create oscillator + gain nodes
+  const oscillatorNodes = [];
+  const oscillatorGainNodes = [];
+  const panners = [];
 
-	// Adjust the panning spread
-	const panSpread = 0.5;
+  // Adjust the panning spread
+  const panSpread = 0.5;
 
-	for (let i = 0; i < numOscillators; i++) {
-		const oscillatorNode = new Tone.OmniOscillator("A4", "pwm").start();
-		// const oscillatorNode = new Tone.Oscillator("A4", "square").start();
+  for (let i = 0; i < numOscillators; i++) {
+    const oscillatorNode = new Tone.OmniOscillator("A4", "pwm").start();
+    // const oscillatorNode = new Tone.Oscillator("A4", "square").start();
 
-		oscillatorNode.volume.value = -24;
-		oscillatorNodes.push(oscillatorNode);
+    oscillatorNode.volume.value = -24;
+    oscillatorNodes.push(oscillatorNode);
 
-		const oscillatorGainNode = new Tone.Gain(0);
-		oscillatorGainNodes.push(oscillatorGainNode);
+    const oscillatorGainNode = new Tone.Gain(0);
+    oscillatorGainNodes.push(oscillatorGainNode);
 
-		const panner = new Tone.Panner(0);
+    const panner = new Tone.Panner(0);
 
-		let panning;
-		if (i === 0) {
-			panning = 0; // Center the first osc
-		} else {
-			const panStep = panSpread / (numOscillators - 1);
-			panning = i * panStep - panSpread / 2;
-		}
+    let panning;
+    if (i === 0) {
+      panning = 0; // Center the first osc
+    } else {
+      const panStep = panSpread / (numOscillators - 1);
+      panning = i * panStep - panSpread / 2;
+    }
 
-		// const panning = (i / (numOscillators - 1)) * 2 - 1; // Range from -1 to 1
-		panner.pan.value = panning;
+    // const panning = (i / (numOscillators - 1)) * 2 - 1; // Range from -1 to 1
+    panner.pan.value = panning;
 
-		panners.push(panner);
-	}
+    panners.push(panner);
+  }
 
-	// create other nodes
-	const oscillatorsSumGainNode = new Tone.Gain(1);
-	const delayNode = new Tone.PingPongDelay("16n", 0.2);
-	const filterNode = new Tone.Filter(1000, "lowpass");
+  console.log(`Created: ${numOscillators} oscillators`);
 
-	//connections
-	oscillatorNodes.forEach((oscillatorNode, i) => {
-		oscillatorNode.chain(
-			oscillatorGainNodes[i],
-			panners[i],
-			oscillatorsSumGainNode
-		);
-	});
+  // create other nodes
+  const oscillatorsSumGainNode = new Tone.Gain(1);
+  const delayNode = new Tone.PingPongDelay("16n", 0.2);
+  const filterNode = new Tone.Filter(1000, "lowpass");
 
-	oscillatorsSumGainNode.connect(delayNode);
-	delayNode.connect(filterNode);
+  //connections
+  oscillatorNodes.forEach((oscillatorNode, i) => {
+    oscillatorNode.chain(
+      oscillatorGainNodes[i],
+      panners[i],
+      oscillatorsSumGainNode
+    );
+  });
 
-	filterNode.toDestination();
+  oscillatorsSumGainNode.connect(delayNode);
+  delayNode.connect(filterNode);
 
-	// Kick pattern
-	const kickPattern = new Tone.Pattern(
-		(time, note) => {
-			kickPlayer.start(time); // Trigger the kick sample
-		},
-		["C1"], // The kick sample will always play at C1
-		"up" // Set the pattern direction
-	);
+  filterNode.toDestination();
 
-	// Set the kick pattern to play on every quarter note
-	kickPattern.interval = "4n";
-	kickPattern.start();
+  // Kick pattern
+  const kickPattern = new Tone.Pattern(
+    (time, note) => {
+      kickPlayer.start(time); // Trigger the kick sample
+    },
+    ["C1"], // The kick sample will always play at C1
+    "up" // Set the pattern direction
+  );
 
-	// Start the kick pattern when the transport starts
-	Tone.Transport.start();
+  // Set the kick pattern to play on every quarter note
+  kickPattern.interval = "4n";
+  kickPattern.start();
 
-	return {
-		//References to nodes, can be nodes, arrays of nodes or objects with nodes as values
-		nodes: {
-			oscillatorNodes,
-			oscillatorGainNodes,
-			oscillatorsSumGainNode,
-			filterNode,
-			panners,
-			delayNode,
-			kickPlayer,
-			kickPattern,
-		},
+  // Start the kick pattern when the transport starts
+  Tone.Transport.start();
 
-		numOscillators,
+  return {
+    //References to nodes, can be nodes, arrays of nodes or objects with nodes as values
+    nodes: {
+      oscillatorNodes,
+      oscillatorGainNodes,
+      oscillatorsSumGainNode,
+      filterNode,
+      panners,
+      delayNode,
+      kickPlayer,
+      kickPattern,
+    },
 
-		setOscillatorGainsFromNormalizedValue(v, rampTime = 0.1) {
-			console.log("setOscillatorGainsFromNormalizedValue");
+    numOscillators,
+    currentChord: [],
 
-			this.nodes.oscillatorGainNodes.forEach((gainNode, i) => {
-				//the first oscGain is always 1
-				let gain = 1;
+    getOscillatorGains() {
+      return this.nodes.oscillatorGainNodes.map(
+        (gainNode) => gainNode.gain.value
+      );
+    },
 
-				if (i > 0) {
-					let unnormalized = 1 + v * (this.numOscillators - 1);
-					gain = clamp(unnormalized - i);
-				}
+    setOscillatorGainsFromNormalizedValue(v, rampTime = 0.1) {
+      console.log("setOscillatorGainsFromNormalizedValue");
 
-				gainNode.gain.rampTo(gain, rampTime);
-			});
-		},
+      const gains = []; //this is just to be able to return the gains, mostly for debug purposes
 
-		setChord(notes, rampTime = 0.1) {
-			const rndDetuneRange = 10;
+      this.nodes.oscillatorGainNodes.forEach((gainNode, i) => {
+        let thisGain;
+        let thisRampTime = rampTime;
 
-			console.log("setChord");
+        if (i > this.currentChord.length || this.currentChord.length === 0) {
+          thisGain = 0;
+          thisRampTime *= 2; //longer fade time when fading to 0
+        } else {
+          let unnormalized = 1 + v * (this.currentChord.length - 1);
+          thisGain = clamp(unnormalized - i);
+        }
 
-			for (const [i, note] of notes.entries()) {
-				if (i > this.numOscillators - 1) break;
+        gainNode.gain.rampTo(thisGain, thisRampTime);
+        gains.push(thisGain);
+      });
 
-				this.nodes.oscillatorNodes[i].frequency.rampTo(note, rampTime);
-				this.nodes.oscillatorNodes[i].detune.rampTo(
-					Math.random() * rndDetuneRange,
-					rampTime
-				);
-			}
-		},
+      return gains;
+    },
 
-		//Dispose ALL nodes
-		dispose() {
-			const _dispose = (arr) => {
-				arr.forEach((node) => {
-					if ("dispose" in node) {
-						node.dispose();
-					} else {
-						_dispose(Array.isArray(node) ? node : Object.values(node));
-					}
-				});
-			};
+    setChord(chord, rampTime = 0.1) {
+      this.currentChord = chord;
 
-			_dispose(Object.values(this.nodes));
+      const rndDetuneRange = 10;
 
-			console.log("—— AUDIO ENGINE: DISPOSED ——");
-		},
-	};
+      console.log("setChord");
+
+      for (const [i, note] of chord.entries()) {
+        if (i > this.numOscillators - 1) break;
+
+        this.nodes.oscillatorNodes[i].frequency.rampTo(note, rampTime);
+        this.nodes.oscillatorNodes[i].detune.rampTo(
+          Math.random() * rndDetuneRange,
+          rampTime
+        );
+      }
+    },
+
+    //Dispose ALL nodes
+    dispose() {
+      const _dispose = (arr) => {
+        arr.forEach((node) => {
+          if ("dispose" in node) {
+            node.dispose();
+          } else {
+            _dispose(Array.isArray(node) ? node : Object.values(node));
+          }
+        });
+      };
+
+      _dispose(Object.values(this.nodes));
+
+      console.log("—— AUDIO ENGINE: DISPOSED ——");
+    },
+  };
 }
 
 export { createAudioEngine };
--- src/audio/chords.js
+++ src/audio/chords.js
@@ -1,32 +1,43 @@
 const chordSets = [
-	[
-		["C3", "E3", "G3", "C4", "C5"],
-		["F3", "A3", "C4", "F4", "F5"],
-		["G3", "B3", "D4", "G4", "G5"],
-		["A3", "C4", "E4", "A4", "A5"],
-	],
-	[
-		["F2", "A3", "C3", "E3", "F3", "A3", "C4", "E4"],
-		["C2", "G2", "C3", "E3", "G3", "B3", "E4", "G4"],
-	],
+  //SPECIAL SET TO TEST DIFF CHORD SIZES
+  [
+    ["F2", "A3", "C3", "E3", "F3", "A3", "C4", "D5"],
+    ["C2", "G2", "C3", "E3", "G3", "B3", "E4", "G4"],
+    ["A2", "C3", "G5"],
+    [],
+  ],
+  [
+    ["C3", "E3", "G3", "C4", "C5"],
+    ["F3", "A3", "C4", "F4", "F5"],
+    ["G3", "B3", "D4", "G4", "G5"],
+    ["A3", "C4", "E4", "A4", "A5"],
+  ],
+  [
+    ["F2", "A3", "C3", "E3", "F3", "A3", "C4", "E4"],
+    ["C2", "G2", "C3", "E3", "G3", "B3", "E4", "G4"],
+  ],
 ];
 
 let chords = chordSets[0];
 
 const changeChordSet = (index) => {
-	if (index >= 0 && index < chordSets.length) {
-		chords = chordSets[index];
-	}
+  if (index >= 0 && index < chordSets.length) {
+    chords = chordSets[index];
+  }
 };
 
 const normalizedToIndex = (v, length) => {
-	if (v === 1) return length - 1;
+  if (v === 1) return length - 1;
 
-	return Math.floor(v * chords.length);
+  return Math.floor(v * chords.length);
 };
 
 const getChord = (v) => chords[normalizedToIndex(v, chords.length)];
 
-const getMinVoices = () => Math.min(...chords.map((chord) => chord.length));
+const getMinVoices = () =>
+  Math.min(...chordSets.flat().map((chord) => chord.length));
 
-export { getChord, getMinVoices, changeChordSet };
+const getMaxVoices = () =>
+  Math.max(...chordSets.flat().map((chord) => chord.length));
+
+export { getChord, getMinVoices, getMaxVoices, changeChordSet };
